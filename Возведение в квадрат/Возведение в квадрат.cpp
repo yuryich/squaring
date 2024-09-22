bfc666a5cfd225f9b4aa23dce6d3b3cc4519630c
Neto test <yury120405@gmail.com>
2024-09-22 22:13:16 +0400

    Initial commit

---
+++ Возведение в квадрат/Возведение в квадрат.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+template <typename T>
+T square(const T& value) {
+	return value * value;
+}
+
+template <typename T>
+vector<T> square(const vector<T>& vec) {
+	vector<T> result(vec.size());
+	transform(vec.begin(), vec.end(), result.begin(), [](const T& val) {
+		return square(val);
+	});
+	return result;
+}
+
+int main() {
+	int num = 4;
+	cout << "Квадрат числа " << num << ": " << square(num) << endl;
+
+	vector<int> vec = { -1, 4, 8 };
+	vector<int> squaredVec = square(vec);
+
+	cout << "Квадраты вектора: ";
+	for (const auto& val : squaredVec) {
+		cout << val << " ";
+	}
+	cout << endl;
+}
